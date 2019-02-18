3af5d1b10d1a82d38b0adbc3f36a1424d549577f
Katarina Tretter <kft3635@ad.rit.edu>
2019-02-18 15:16:27 -0500

    Changed line 8

--- HatQuest/HatQuest/Game1.cs
+++ HatQuest/HatQuest/Game1.cs
@@ -5,7 +5,7 @@ using Microsoft.Xna.Framework.Input;
 namespace HatQuest
 {
     /// <summary>
-    /// This is the main type for your game.
+    /// MEOW - Kat
     /// </summary>
     public class Game1 : Game
     {
