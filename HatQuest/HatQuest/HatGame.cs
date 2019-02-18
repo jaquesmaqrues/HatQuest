88648c282576ccf445b9fb3c8387e5cad093831a
eah2383 <eah2383@IGMOH1380-04>
2019-02-18 15:17:10 -0500

    Super broken commit

--- HatQuest/HatQuest/HatGame.cs
+++ HatQuest/HatQuest/HatGame.cs
@@ -7,12 +7,12 @@ namespace HatQuest
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class HatGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public HatGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
