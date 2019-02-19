a0412dfeebf5220307768103393b4345fe68e201
eah2383 <eah2383@MSS2175-17>
2019-02-18 20:44:54 -0500

    base FSM commit

---
+++ HatQuest/HatQuest/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HatQuest
+{
+    class Menu
+    {
+        public Menu()
+        {
+
+        }
+
+        public MainState Update()
+        {
+            return MainState.Menu;
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+
+        }
+    }
+}
