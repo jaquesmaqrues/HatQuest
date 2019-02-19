7060436963ee3e8524782e76a8a7999b043b2b61
elijah.hughes99 <elijah.hughes99@gmail.com>
2019-02-19 10:31:00 -0500

    Started building Play class FSM

--- HatQuest/HatQuest/Play.cs
+++ HatQuest/HatQuest/Play.cs
@@ -9,8 +9,16 @@ using Microsoft.Xna.Framework.Input;
 
 namespace HatQuest
 {
+    enum PlayState { PlayerTurn, EnemyTurn, SafeRoom }
+
     class Play
     {
+        //Fields
+        private Player player;
+        private Queue<Room> floor;
+        private PlayState state;
+        private int floorLevel;
+
         public Play()
         {
 
@@ -18,10 +26,35 @@ namespace HatQuest
 
         public MainState Update()
         {
+            switch(state)
+            {
+                case PlayState.PlayerTurn:
+                    break;
+                case PlayState.EnemyTurn:
+                    break;
+                case PlayState.SafeRoom:
+                    break;
+            }
             return MainState.Play;
         }
 
         public void Draw(SpriteBatch batch)
+        {
+            switch (state)
+            {
+                case PlayState.PlayerTurn:
+                    break;
+                case PlayState.EnemyTurn:
+                    break;
+                case PlayState.SafeRoom:
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Generates a new set of rooms for the floor
+        /// </summary>
+        private void GenerateRoom()
         {
 
         }
---
+++ HatQuest/HatQuest/Room.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace HatQuest
+{
+    class Room
+    {
+        //Fields
+        private Enemy[] enemies;
+        private int currentAttacker;
+        private float timer;
+
+        //Properties
+        public Enemy this[int i]
+        {
+            get { return enemies[i]; }
+        }
+
+        public Room(Enemy en1, Enemy en2 = null, Enemy en3 = null, Enemy en4 = null, Enemy en5 = null)
+        {
+            enemies = new Enemy[5];
+            enemies[0] = en1;
+            enemies[1] = en2;
+            enemies[2] = en3;
+            enemies[3] = en4;
+            enemies[4] = en5;
+        }
+
+        /// <summary>
+        /// Used as an Update method for the Room class while the Play class is in the EnemyTurn state
+        /// </summary>
+        /// <param name="player">The current player for the enemies to target</param>
+        public void TakeEnemyTurn(Player player)
+        {
+
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+
+        }
+    }
+}
