90a9fa55843fe8121bf94005b9bffee66bd558ad
jm1124 <jm1124@g.rit.edu>
2019-02-19 23:04:15 -0500

    Changed init classes to directories

--- HatQuest/HatQuest/Directories/AbilitiesDirectory.cs
+++ HatQuest/HatQuest/Directories/AbilitiesDirectory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HatQuest.Init
 {
-    static class Abilities
+    static class AbilitiesDirectory
     {
         //This is where we could store a list of all abilities so  they only have to be made once and all entities
         //  just have refrences to the ability in this class
--- HatQuest/HatQuest/Directories/RoomsDirectory.cs
+++ HatQuest/HatQuest/Directories/RoomsDirectory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HatQuest.Init
 {
-    class Rooms
+    class RoomsDirectory
     {
         //We could use this class to store all the room layouts then use it to generate new rooms with copies
         //  of these layouts. This would also mean we need a new RoomLayout class
