c62874859be1f28d075a09058cbe45e8b42e1927
elijah.hughes99 <elijah.hughes99@gmail.com>
2019-02-19 22:53:16 -0500

    More super useful architexture that doesnt do anything yet

---
+++ HatQuest/HatQuest/Hats/Hat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatQuest.Hats
+{
+    class Hat
+    {
+        //Cool things
+    }
+}
