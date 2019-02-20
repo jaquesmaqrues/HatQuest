66a407841981df3c74df92dfef5815d095ab1b5e
Iain <icd1636@g.rit.edu>
2019-02-19 23:25:20 -0500

    Added Hat Stats

--- HatQuest/HatQuest/Hats/Hat.cs
+++ HatQuest/HatQuest/Hats/Hat.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using HatQuest.Init;
 
 namespace HatQuest.Hats
 {
     class Hat
     {
-        //Cool things
+        int health;
+        int maxHealth;
+        int def;
+        int atk;
+        int mana;
+        int maxMana;       
     }
 }
