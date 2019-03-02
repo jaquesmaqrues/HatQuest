88303663a1531d2e6b6c24a0a7e89c7b7959d4f9
icd1636 <icd1636@MSS3110-29>
2019-03-02 18:49:04 -0500

    Added a Vampire Bat into the EnemiesDirectory that uses the LifeSiphon ability

--- HatQuest/HatQuest/Directories/EnemiesDirectory.cs
+++ HatQuest/HatQuest/Directories/EnemiesDirectory.cs
@@ -15,5 +15,6 @@ namespace HatQuest.Init
         //Example of how it would be declared (except for the null ability list we just dont have those yet lol)
         public static EnemyType GOBLIN = new EnemyType("Goblin", SpritesDirectory.GetSprite("Goblin"), 10, 2, 5, new Ability[] { AbilitiesDirectory.ATTACK });
         public static EnemyType FORKGNOME = new EnemyType("Fork-Gnome", SpritesDirectory.GetSprite("Goblin"), 5, 4, 2, new Ability[] { AbilitiesDirectory.QUICKATTACK, AbilitiesDirectory.ATTACK });
+        public static EnemyType VAMPIREBAT = new EnemyType("Vampire Bat", SpritesDirectory.GetSprite("Goblin"), 5, 4, 1, new Ability[] { AbilitiesDirectory.ATTACK, AbilitiesDirectory.LIFESIPHON });
     }
 }
