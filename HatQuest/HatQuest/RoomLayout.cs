886c1820186efd77e3c67b6354f809f41afeb51d
eah2383 <eah2383@MSS3110-31>
2019-03-02 19:23:15 -0500

    IDK what's changed it just says i need to commit

---
+++ HatQuest/HatQuest/RoomLayout.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatQuest
+{
+    class RoomLayout
+    {
+        //Fields
+        private EnemyType[] enemies;
+
+        //Properties
+        public EnemyType this[int i]
+        {
+            get
+            {
+                if(i >= 0 && i < 5)
+                {
+                    return enemies[i];
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="enemy1">First enemy</param>
+        /// <param name="enemy2">Second enemy</param>
+        /// <param name="enemy3"></param>
+        /// <param name="enemy4"></param>
+        /// <param name="enemy5"></param>
+        public RoomLayout(EnemyType enemy1, EnemyType enemy2, EnemyType enemy3, EnemyType enemy4, EnemyType enemy5)
+        {
+            enemies = new EnemyType[] { enemy1, enemy2, enemy3, enemy4, enemy5 };
+        }
+    }
+}
