80c4ad9d1d97a94d756fe86c83639f0a0068b62b
icd1636 <icd1636@g.rit.edu>
2019-03-04 14:30:57 -0500

    HatsDirectory: Added tiers to hats, and added a bunch of F-Tier Hats

--- HatQuest/HatQuest/Directories/HatsDirectory.cs
+++ HatQuest/HatQuest/Directories/HatsDirectory.cs
@@ -10,7 +10,13 @@ namespace HatQuest.Init
     static class HatsDirectory
     {
         //Yall already know what it is bois
+        //---- F-Tier Hats ----//
         public static Hat ATKHAT = new Hat("Attack Hat", "A hat that boosts your attack", SpritesDirectory.GetSprite("Hat"), 0, 0, 5);
+        public static Hat HPHAT = new Hat("Health Hat", "A hat that boosts your health", SpritesDirectory.GetSprite("Hat"), 5);
+        public static Hat DEFHAT = new Hat("Defense Hat", "A hat that boosts your defense", SpritesDirectory.GetSprite("Hat"), 0, 5);
+        public static Hat MANAHAT = new Hat("Mana Hat", "A hat that boosts your mana", SpritesDirectory.GetSprite("Hat"), 0, 0, 0, 5);
+
+        //---- S-Tier Hats ----//
         public static Hat BUCKETHAT = new BucketHat(SpritesDirectory.GetSprite("Hat"));
 
         //Erin's hat
