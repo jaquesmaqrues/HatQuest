f359155934bcc97726d0e6a3ccdde24cb73b2611
Katarina Tretter <kft3635@ad.rit.edu>
2019-04-26 15:52:54 -0400

    Fixed error in Room

--- HatQuest/HatQuest/Room.cs
+++ HatQuest/HatQuest/Room.cs
@@ -55,7 +55,7 @@ namespace HatQuest
                     }
                     else
                     {
-                        enemies[k] = new Enemy(layout[k], level, new Point(550, (k * 60) + 60), 75, 150, player, , layout[k].HatPosition);
+                        enemies[k] = new Enemy(layout[k], level, new Point(550, (k * 60) + 60), 75, 150, player, layout[k].HatPosition);
                         enemies[k].Name += (" " + (k + 1));
                     }
                 }
